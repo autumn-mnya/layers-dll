6ba333977f40fb60c7444d21dd778ad7323639b6
Autumn <49522306+autumn-mnya@users.noreply.github.com>
2024-04-29 09:29:22 -0400

    add files

---
+++ src/Main.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+
+#include "mod_loader.h"
+#include "cave_story.h"
+
+void InitMod(void)
+{
+
+}
